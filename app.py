31547d321d98eabd8e47da738290ef8e52ac9d4d
Foryoucom <godud0074@gmail.com>
2025-05-30 02:55:27 +0900

    fix: Render용 포트 설정 추가

--- app.py
+++ app.py
@@ -64,5 +64,9 @@ def diary():
 
     return render_template('diary.html', posts=posts)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
